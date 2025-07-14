57ad2405328ee793af31c20372a8441ac961f34b
Rong <rongrubaiya@gmail.com>
2025-07-14 08:56:13 +0600

    done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const inputArea=document.getElementById('inputSide');
 const button=document.getElementById('btn');
 const todoList=document.getElementById('to-do-li');
 
+let editTodo=null;
+
 
 const addTodo =()=>{
   const inputStore=inputArea.value.trim();
@@ -11,7 +13,16 @@ const addTodo =()=>{
     // !if nothing write the next function will not applied
     return false;  
   }
-
+  //  ! edit and remove btn function
+  
+  if(button.innerText === "Edit"){
+    editTodo.target.previousElementSibling.innerHTML=inputStore;
+    button.innerText="Add"
+    inputArea.value="";
+     editTodo = null;
+  }
+  
+  else{
   // !  li items appeared
   const li=document.createElement('li');  
   const p=document.createElement('p');
@@ -37,12 +48,23 @@ const addTodo =()=>{
   delButton.innerText="Remove";
   delButton.classList.add('todoListBtn')/* //!added class for styling */
   li.appendChild(delButton);
-}
+
+  todoList.appendChild(li);
+  inputArea.value="";
+}}
 
 // ! remove and edit button running function
 const updateTodo=(e)=>{
   if (e.target.innerHTML==="Remove"){
     todoList.removeChild(e.target.parentElement);
+  }
+
+  if(e.target.innerHTML==="Edit"){
+    inputArea.value=e.target.previousElementSibling.innerHTML;
+    inputArea.focus();
+    button.innerText="Edit";
+    editTodo=e;
+
   }
 }
 
